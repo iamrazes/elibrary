8725bb92e220b37ab1deda6b167b6dbca2187686
Muhammad Dagistan Silawane <mochrazesds@gmail.com>
2022-01-18 13:58:45 +0700

    checkpoint beta

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,39 +1,35 @@
-@include('layouts.adminlte.head')
+@extends('layouts.adminlte')
 
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
+@section('content')
 
-@include('layouts.adminlte.navbar')
-
-@include('layouts.adminlte.sidebar')
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">DATABASE USERS</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="{{url('/admin')}}">Home</a></li>
-              <li class="breadcrumb-item active">Database Users</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">DATABASE USERS</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{url('/admin')}}">Home</a></li>
+                        <li class="breadcrumb-item active">Database Users</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
 
     <!-- Main content -->
 
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+</div>
+<!-- /.content-wrapper -->
 
+@endsection
 
-@include('layouts.adminlte.script')
 </body>
+
 </html>
